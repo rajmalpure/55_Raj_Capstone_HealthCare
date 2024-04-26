7720b16e739c58b0b9fbe3add645fab0379364d8
rajmalpure04 <rajmalpure14@gmail.com>
2024-04-26 13:36:08 +0530

    GET API

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PUBLIC_PORT || 3000;
+
+// define the ping route
+app.get('/ping', (req, res) => {
+  res.send("Hello");
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
+
+module.exports = app;
